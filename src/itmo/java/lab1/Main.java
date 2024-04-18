44b12ba175cec3883619647d739670a8026be2f6
Akhmad Bakir <akhmgazbak@gmail.com>
2024-04-18 22:46:53 +0300

    first commit

---
+++ src/itmo/java/lab1/Main.java
@@ -0,0 +1,14 @@
+package itmo.java.lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java.");
+
+
+
+    }
+}
