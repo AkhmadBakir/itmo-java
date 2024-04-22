90da8845de11d76cec0d8d77bbf82e7a350139a2
Akhmad Bakir <akhmgazbak@gmail.com>
2024-04-22 09:06:31 +0300

    Task_4 is added

--- src/itmo/java/lab4/Main.java
+++ src/itmo/java/lab4/Main.java
@@ -1,4 +1,10 @@
 package itmo.java.lab4;
 
 public class Main {
+//    Integer i;
+    for (int i = 1; i <= 100; i++) {
+        if (i%2 !=0) {
+            System.out.println(i);
+        }
+    }
 }
