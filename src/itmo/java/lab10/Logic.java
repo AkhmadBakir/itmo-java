acd2ae7fcd2dde96441b6a8cbc70983490689b1a
Akhmad Bakir <akhmgazbak@gmail.com>
2024-05-05 00:02:31 +0300

    Task_10 has been added

---
+++ src/itmo/java/lab10/Logic.java
@@ -0,0 +1,71 @@
+package itmo.java.lab10;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Logic {
+
+    //    Первое упражнение
+
+    public static List<String> myMethod(File file1) {
+        String myString;
+        List<String> myList = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file1))) {
+            while ((myString = reader.readLine()) != null) {
+                myList.add(myString);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return myList;
+    }
+
+//    Второе упражнение
+
+    public static void writeString(String string, File file2) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file2))) {
+            writer.write(string);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+//    Третье упражнение
+
+    public static void myMethod1(File file1, File file2, File file3) {
+        try (BufferedReader reader1 = new BufferedReader(new FileReader(file1));
+             BufferedReader reader2 = new BufferedReader(new FileReader(file2));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(file3))) {
+            String string1, string2 = null;
+            while ((string1 = reader1.readLine()) != null) {
+                writer.write(string1);
+            }
+            while ((string2 = reader2.readLine()) != null) {
+                writer.write(string2);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+//    Четвертое упражнение
+
+    public static void myMethod2(File file1) {
+        File tempFile = new File("files/tempFile.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file1));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
+            String string = null;
+            while ((string = reader.readLine()) != null) {
+                String string1 = string.replace(" ", "$");
+                System.out.println(string1);
+                writer.write(string1);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        tempFile.renameTo(new File("file1.txt"));
+    }
+}
--- src/itmo/java/lab10/Main.java
+++ src/itmo/java/lab10/Main.java
@@ -1,7 +1,33 @@
 package itmo.java.lab10;
+import java.io.File;
+import java.util.*;
+
+import static itmo.java.lab10.Logic.*;
+
 
 public class Main {
     public static void main(String[] args) {
 
+//        Первое упражнение
+
+        File file1 = new File("files/file1.txt");
+//        List<String> myList1 = myMethod(file1);
+//        System.out.println(myList1);
+
+//        Второе упражнение
+
+//        String string = "Hello world!";
+//        File file2 = new File("files/file2.txt");
+//        writeString(string, file2);
+
+//        Третье упражнение
+
+//        File file3 = new File("files/file3.txt");
+//        myMethod1(file1, file2, file3);
+
+//        Четвертое упражнение
+
+
+        myMethod2(file1);
     }
 }
